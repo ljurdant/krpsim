6c39a7b35352b16dc0d5b9378906bcb107a92005
ljurdant <ljurdant@student.42.fr>
2025-04-11 16:26:19 +0200

    Enhance genetic algorithm with new crossover methods and tournament selection parameters; update population initialization and configuration settings

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -13,6 +13,10 @@ class GeneticAlgorithm:
         selection_rate=0.5,
         elite_rate=0.0,
         generations=100,
+        selection_pressure=2,
+        tournament_probability=0.9,
+        parent_selection_type="random",
+        crossover_point="single",
         genes=None,
         fitness_function=None,
         init_population=None,
@@ -28,6 +32,10 @@ class GeneticAlgorithm:
         self.fitness_function = fitness_function
         self.init_population = init_population
         self.elite_rate = elite_rate
+        self.selection_pressure = selection_pressure
+        self.tournament_probability = tournament_probability
+        self.parent_selection_type = parent_selection_type
+        self.crossover_point = crossover_point
         self.min_dna_length = min_dna_length
         self.max_dna_length = max_dna_length
         self.genes = genes if genes is not None else []
@@ -46,15 +54,16 @@ class GeneticAlgorithm:
         elite_count = int(self.population_size * self.elite_rate)
         return self.population[:elite_count]
 
-    def crossover(self, individual1, individual2):
-        """Perform crossover between two selected individuals and return two children."""
-        child = []
-        # crossover_point = min(
-        #     random.randint(1, max(len(individual1), len(individual2)) - 1),
-        #     min(len(individual1), len(individual2)) - 1,
-        # )
-        # child = individual1[:crossover_point] + individual2[crossover_point:]
+    def single_point_crossover(self, individual1, individual2):
+        crossover_point = min(
+            random.randint(1, max(len(individual1), len(individual2)) - 1),
+            min(len(individual1), len(individual2)) - 1,
+        )
+        return individual1[:crossover_point] + individual2[crossover_point:]
 
+    def uniform_crossover(self, individual1, individual2):
+        """Perform uniform crossover between two selected individuals and return two children."""
+        child = []
         length = len(individual1)
         if len(individual2) != len(individual1):
             length = random.randint(
@@ -62,7 +71,6 @@ class GeneticAlgorithm:
                 max(len(individual1), len(individual2)),
             )
         for i in range(length):
-
             if i > len(individual1) - 1:
                 child.append(individual2[i])
                 continue
@@ -73,15 +81,33 @@ class GeneticAlgorithm:
 
         return child
 
-    def tournament_selection(self, population, k=2):
+    def crossover(self, individual1, individual2):
+        """Perform crossover between two selected individuals and return two children."""
+        if self.crossover_point == "single":
+            return self.single_point_crossover(individual1, individual2)
+        elif self.crossover_point == "uniform":
+            return self.uniform_crossover(individual1, individual2)
+        else:
+            raise ValueError("Invalid crossover point type.")
+
+    def get_crossover_parent(self, population):
+        """
+        Select two parents for crossover using tournament selection.
+        """
+        if self.parent_selection_type == "tournament":
+            return self.tournament_selection(population)
+        else:
+            return random.choice(population)
+
+    def tournament_selection(self, population):
         """
         Perform a tournament of size k on the population.
         Return the best individual (highest fitness) among k random picks.
         """
 
-        p = 0.9
+        p = self.tournament_probability
         # Randomly choose k individuals
-        tournament_contestants = random.sample(population, k)
+        tournament_contestants = random.sample(population, self.selection_pressure)
         tournament_contestants.sort(
             key=lambda x: self.fitness_function(x), reverse=True
         )
@@ -91,13 +117,14 @@ class GeneticAlgorithm:
         ]
         # Identify best
 
-    def create_offspring(self, population, k=2):
+    def create_offspring(self, population):
         """
         Example function to create one child using tournament selection and crossover.
         """
         # 1) Select parents via tournament
-        parent1 = self.tournament_selection(population, k)
-        parent2 = self.tournament_selection(population, k)
+
+        parent1 = self.get_crossover_parent(population)
+        parent2 = self.get_crossover_parent(population)
 
         if random.random() < self.crossover_rate:
             child = self.crossover(parent1, parent2)
@@ -114,7 +141,7 @@ class GeneticAlgorithm:
         target_size = self.population_size - int(self.population_size * self.elite_rate)
         while len(crossover_population) < target_size:
 
-            child = self.create_offspring(population, 4)
+            child = self.create_offspring(population)
 
             crossover_population.append(self.mutate(child))
 
@@ -134,22 +161,21 @@ class GeneticAlgorithm:
         fitnesses = []
 
         for _ in ft_progress(range(self.generations)):
-
-            fitnesses.append(
-                max([self.fitness_function(ind) for ind in self.population])
-            )
-
-            # parent_population = self.parent_selection()
-
             self.sort_population()
+
+            fitnesses.append(self.fitness_function(self.population[0]))
+
+            parent_population = self.parent_selection()
             elite_population = self.elite_selection()
-            crossover_population = self.crossover_generation(self.population)
+
+            crossover_population = self.crossover_generation(parent_population)
 
             self.population = crossover_population + elite_population
 
             # cleanup population
-        fitnesses.append(max([self.fitness_function(ind) for ind in self.population]))
+        best = max(self.population, key=lambda x: self.fitness_function(x))
+        fitnesses.append(self.fitness_function(best))
         return (
-            max(self.population, key=lambda x: self.fitness_function(x)),
+            best,
             fitnesses,
         )  # Return the best individual
